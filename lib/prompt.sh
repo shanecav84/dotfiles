47944d780546d9858fc83b39f2ad0e0d2ed62bd8
Shane Cavanaugh <shane@patientdev.co>
2016-09-28 12:17:46 -0400

    Many updates from work machine

---
+++ lib/prompt.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+GIT_PROMPT_ONLY_IN_REPO=1
+PS1="$Red>$IPurple>$Yellow>$Green>$Cyan>$Color_Off "
+GIT_PROMPT_START="$Cyan\T"
+GIT_PROMPT_END=" $PS1"
+GIT_PROMPT_COMMAND_OK="blah"
