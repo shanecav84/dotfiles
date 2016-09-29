cf662e91294fd29249bcfde67d1a67bb4f9097c7
Shane Cavanaugh <shane@patientdev.co>
2016-09-29 15:21:37 -0400

    Move ENV variables out of profile

--- lib/prompt.sh
+++ lib/prompt.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
-GIT_PROMPT_ONLY_IN_REPO=1
 PS1="$Red>$IPurple>$Yellow>$Green>$Cyan>$Color_Off "
+
+# bash-git-prompt
+GIT_PS1_SHOWUPSTREAM="auto"
+GIT_PS1_SHOWCOLORHINTS="1"
+GIT_PROMPT_ONLY_IN_REPO=1
 GIT_PROMPT_START="$Cyan\T"
 GIT_PROMPT_END=" $PS1"
 GIT_PROMPT_COMMAND_OK="blah"
