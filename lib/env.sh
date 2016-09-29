39059b1d994012cf07a591345fa3b00aae176038
Shane Cavanaugh <shane@patientdev.co>
2016-09-29 15:21:37 -0400

    Move ENV variables out of profile

--- lib/env.sh
+++ lib/env.sh
@@ -1,5 +1,19 @@
-export EDITOR="nano"
+#!/usr/bin/env bash
+export EDITOR="vi"
+
+# Mah dotfiles
+export PATH=$DOTFILES_PATH/bin:${PATH}
+
+# Homebrew, presumably
+export PATH=$(echo $PATH | sed 's|/usr/local/bin||; s|/usr/local/sbin||; s|::|:|; s|^:||; s|\(.*\)|/usr/local/bin:/usr/local/sbin:\1|')
+
+# Python `virtualenv` stuff
+export WORKON_HOME=$HOME/.virtualenvs
+export PROJECT_HOME=$HOME/Sites/django
 
 # Locales
-export LC_ALL=en_US.UTF-8  
+export LC_ALL=en_US.UTF-8
 export LANG=en_US.UTF-8
+
+# Lunchy gem
+LUNCHY_DIR=$(dirname `gem which lunchy`)/../extras
--- profile.sh
+++ profile.sh
@@ -3,10 +3,6 @@
 # Mah dotfiles
 for f in $DOTFILES_PATH/lib/*.sh; do source $f; done
 
-export PATH=$(echo $PATH | sed 's|/usr/local/bin||; s|/usr/local/sbin||; s|::|:|; s|^:||; s|\(.*\)|/usr/local/bin:/usr/local/sbin:\1|')
-
-GIT_PS1_SHOWUPSTREAM="auto"
-GIT_PS1_SHOWCOLORHINTS="1"
 
 if [ -f "$(brew --prefix)/opt/bash-git-prompt/share/gitprompt.sh" ]; then
     source "$(brew --prefix)/opt/bash-git-prompt/share/gitprompt.sh"
@@ -14,17 +10,12 @@ fi
 
 if which rbenv > /dev/null; then eval "$(rbenv init -)"; fi
 
-export PATH="/usr/local/sbin:$PATH"
-export PATH=$DOTFILES_PATH/bin:${PATH}
-
-# Python virtualenv stuff
-export WORKON_HOME=$HOME/.virtualenvs
-export PROJECT_HOME=$HOME/Sites/django
+# Python `virtualenv` stuff
 if [ -f /usr/local/bin/virtualenvwrapper.sh ]; then source /usr/local/bin/virtualenvwrapper.sh; fi
 
-LUNCHY_DIR=/usr/local/lib/ruby/gems/2.2.0/gems/lunchy-0.10.3/lib/../extras
-   if [ -f /lunchy-completion.bash ]; then
-     . /lunchy-completion.bash
-   fi
+# `lunchy` gem
+if [ -f $LUNCHY_DIR/lunchy-completion.bash ]; then
+ . $LUNCHY_DIR/lunchy-completion.bash
+fi
 
 source git-completion.bash
