790b5c70d56c19721fcde442e6d98e38374ae78b
Shane Cavanaugh <shane@patientdev.co>
2016-09-30 10:52:33 -0400

    Add Jared's rg

--- lib/aliases.sh
+++ lib/aliases.sh
@@ -4,3 +4,6 @@
 alias reload!='. ~/.bash_profile'
 alias today="$DOTFILES_PATH/bin/today.sh"
 alias bunup="$DOTFILES_PATH/bin/bunup.rb"
+
+source "$DOTFILES_PATH/lib/jb_rubygem_page.sh"
+alias rg="jb_rubygem_page"
--- lib/prompt.sh
+++ lib/prompt.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-PS1="$Red>$IPurple>$Yellow>$Green>$Cyan>$Color_Off "
+PS1="$Cyan\W $Red>$IPurple>$Yellow>$Green>$Cyan>$Color_Off "
 
 # bash-git-prompt
 GIT_PS1_SHOWUPSTREAM="auto"
 GIT_PS1_SHOWCOLORHINTS="1"
 GIT_PROMPT_ONLY_IN_REPO=1
-GIT_PROMPT_START="$Cyan\T"
-GIT_PROMPT_END=" $Green\W $PS1"
+GIT_PROMPT_START="$Green\T"
+GIT_PROMPT_END=" $PS1"
 GIT_PROMPT_COMMAND_OK="blah"
 
 PROMPT_COMMAND="today" # Check updates every new primary prompt
