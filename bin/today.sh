bc8ee74ae9eda8a32753fb67a1522aa9295aba3a
Shane Cavanaugh <shane@patientdev.co>
2016-09-30 10:42:49 -0400

    today.sh

---
+++ bin/today.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+last_updated=$(<$DOTFILES_PATH/tmp/today_last_updated.txt)
+now=$(date +%s)
+hours_since=$((($now-$last_updated)/60/60))
+DCS_PATH="$HOME/Dev/dcs/dcs-manager"
+
+brew_output() {
+    printf "# Homebrew\n"
+    brew outdated
+}
+
+bundle_output() {
+    printf "# DCS bundler\n"
+    $DCS_PATH/bin/bundle outdated --parseable
+}
+
+if [[ $hours_since -gt 3 || $1 -eq "now" ]]; then
+    brew_output
+    printf "\n"
+    bundle_output
+fi
