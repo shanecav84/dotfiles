a4410f8d66093446e5c2acd7578c99a104105ac9
Shane Cavanaugh <shane@patientdev.co>
2016-09-30 10:45:53 -0400

    Remove startup.sh

--- profile.sh
+++ profile.sh
@@ -3,8 +3,6 @@
 # Mah dotfiles
 for f in $DOTFILES_PATH/lib/*.sh; do source $f; done
 
-startup.sh
-
 if [ -f "$(brew --prefix)/opt/bash-git-prompt/share/gitprompt.sh" ]; then
     source "$(brew --prefix)/opt/bash-git-prompt/share/gitprompt.sh"
 fi
