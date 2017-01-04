b50e39fb23739e5db43811d0dbfb69eef6e1c8f9
Shane Cavanaugh <shane@patientdev.co>
2017-01-04 15:51:48 -0500

    Aliases and PS1

--- lib/aliases.sh
+++ lib/aliases.sh
@@ -7,8 +7,10 @@ alias today="$DOTFILES_PATH/bin/today.sh"
 # Update Homebrew
 alias brewup='brew update && brew upgrade && brew cleanup'
 
+alias brs="bin/rails s"
 alias bunup="$DOTFILES_PATH/bin/bunup.rb"
 alias cdcs="cd ~/Dev/dcs/dcs-manager"
+alias cdj="cd ~/Dev/classroomdj"
 alias reset="clear && printf '\e[3J'"
 
 source "$DOTFILES_PATH/lib/jb_rubygem_page.sh"
