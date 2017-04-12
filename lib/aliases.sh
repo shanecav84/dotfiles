e414ad22023bc44d9bc75179757a8a6491c21048
Shane Cavanaugh <shane@patientdev.co>
2017-04-12 12:03:07 -0400

    Update to Code dir

--- lib/aliases.sh
+++ lib/aliases.sh
@@ -9,8 +9,8 @@ alias brewup='brew update && brew upgrade && brew cleanup'
 
 alias brs="bin/rails s"
 alias bunup="$DOTFILES_PATH/bin/bunup.rb"
-alias cdcs="cd ~/Dev/dcs/dcs-manager"
-alias cdj="cd ~/Dev/classroomdj"
+alias cdcs="cd ~/Code/dcs/dcs-manager"
+alias cdj="cd ~/Code/classroomdj"
 alias reset="clear && printf '\e[3J'"
 
 source "$DOTFILES_PATH/lib/jb_rubygem_page.sh"
