be7e93f60bebbe35c9de337fea08a3a20d5ed222
Shane Cavanaugh <shane@shanecav.net>
2017-10-21 13:47:39 -0400

    Remove unused profile theme and better standardize indentation

--- profile.sh
+++ profile.sh
@@ -6,25 +6,29 @@ for f in $DOTFILES_PATH/lib/*.sh; do source $f; done
 # ================ Bash Completion =========================
 
 if [ -f /usr/local/etc/bash_completion ]; then
-  . /usr/local/etc/bash_completion
+    . /usr/local/etc/bash_completion
 fi
 
 # ================ Git Prompt ==============================
 # https://github.com/magicmonty/bash-git-prompt
 
 if [ -f "$(brew --prefix)/opt/bash-git-prompt/share/gitprompt.sh" ]; then
-  __GIT_PROMPT_DIR=$(brew --prefix)/opt/bash-git-prompt/share
-  source "$(brew --prefix)/opt/bash-git-prompt/share/gitprompt.sh"
+    __GIT_PROMPT_DIR=$(brew --prefix)/opt/bash-git-prompt/share
+    source "$(brew --prefix)/opt/bash-git-prompt/share/gitprompt.sh"
 fi
 
 if which rbenv > /dev/null; then eval "$(rbenv init -)"; fi
 
 # Python `virtualenv` stuff
-if [ -f /usr/local/bin/virtualenvwrapper.sh ]; then source /usr/local/bin/virtualenvwrapper.sh; fi
+VIRTUALENVWRAPPER_PYTHON=/usr/local/bin/python3
+if [ -f /usr/local/bin/virtualenvwrapper.sh ]; then 
+    source /usr/local/bin/virtualenvwrapper.sh; 
+fi
 
 # `lunchy` gem
 if [ -f $LUNCHY_DIR/lunchy-completion.bash ]; then
- . $LUNCHY_DIR/lunchy-completion.bash
+   . $LUNCHY_DIR/lunchy-completion.bash
 fi
 
 source $DOTFILES_PATH/git-completion.bash
+source ~/.env
