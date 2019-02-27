5bb3f52c10a89a466b169aa6965325a8fca567fc
Shane Cavanaugh <shane@shanecav.net>
2019-02-27 00:41:41 -0500

    Remove redundant comment

--- profile.sh
+++ profile.sh
@@ -3,7 +3,6 @@
 # Mah dotfiles
 for f in $DOTFILES_PATH/lib/*.sh; do source $f; done
 
-# ================ Bash Completion =========================
 if [ -f /usr/local/etc/bash_completion ]; then . /usr/local/etc/bash_completion; fi
 if which rbenv > /dev/null; then eval "$(rbenv init -)"; fi
 
