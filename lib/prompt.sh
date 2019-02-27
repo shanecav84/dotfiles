d2feabbb09b021566bec3fd9c1eb2f30f112c0ec
Shane Cavanaugh <shane@shanecav.net>
2019-02-27 00:41:30 -0500

    Replace git prompt with custom

--- lib/prompt.sh
+++ lib/prompt.sh
@@ -1,12 +1,34 @@
 #!/usr/bin/env bash
 
-PS1="$Cyan\W$Color_Off\n$Red>$Color_Off$IPurple>$Color_Off$Yellow>$Color_Off$Green>$Color_Off$Cyan>$Color_Off "
+# https://gist.github.com/jstn/d249cc4bbb76e0e06ccc6d6a11ff5f04
+function git_prompt() {
+  local dirty=false
 
-# bash-git-prompt
-GIT_PS1_SHOWUPSTREAM="auto"
-GIT_PS1_SHOWCOLORHINTS="1"
-GIT_PROMPT_ONLY_IN_REPO=1
-IT_PROMPT_START="$Green\T"
-GIT_PROMPT_START="\@"
-GIT_PROMPT_END=" $PS1"
-GIT_PROMPT_COMMAND_OK="blah"
+  if [[ $(git status --short 2> /dev/null | wc -l) -gt 0 ]]; then
+    dirty=true
+  fi
+
+  if ! git diff --quiet 2> /dev/null; then
+    dirty=true
+  fi
+
+  local ref
+  ref=$(git symbolic-ref --quiet HEAD 2> /dev/null)
+  local ret=$?
+  if [[ $ret != 0 ]]; then
+    [[ $ret == 128 ]] && return
+    ref=$(git rev-parse --short HEAD 2> /dev/null) || return
+  fi
+
+  if [ "$dirty" = true ]; then
+    echo " [$Yellow${ref#refs/heads/}$Color_Off]"
+  else
+    echo " [${ref#refs/heads/}]"
+  fi
+}
+
+function prompt() {
+    PS1="\@ $Cyan\W$Color_Off$(git_prompt) $Red>$Color_Off$IPurple>$Color_Off$Yellow>$Color_Off$Green>$Color_Off$Cyan>$Color_Off "
+}
+
+PROMPT_COMMAND=prompt
