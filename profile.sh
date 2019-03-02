9eb0dc4d44d3fe3518faf4addc4699cb962e7edd
Shane Cavanaugh <shane@shanecav.net>
2019-03-02 09:49:18 -0500

    Use distributed git-completion script

--- profile.sh
+++ profile.sh
@@ -3,7 +3,11 @@
 # Mah dotfiles
 for f in $DOTFILES_PATH/lib/*.sh; do source $f; done
 
-if [ -f /usr/local/etc/bash_completion ]; then . /usr/local/etc/bash_completion; fi
+if [ -f /usr/local/opt/git/etc/bash_completion.d/git-completion.bash ]; then
+  . /usr/local/opt/git/etc/bash_completion.d/git-completion.bash
+else
+  echo "Expected this machine to have git-completion at /usr/local/opt/git/etc/bash_completion.d/git-completion.bash"
+fi
+
 if which rbenv > /dev/null; then eval "$(rbenv init -)"; fi
 
-source $DOTFILES_PATH/git-completion.bash
