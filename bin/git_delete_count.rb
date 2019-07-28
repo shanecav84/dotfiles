11c2a66dcc2530ec28bb723189282c042c8cd6f2
Shane Cavanaugh <shane@shanecav.net>
2019-07-28 09:31:55 -0400

    Switch to case statement

--- bin/git_delete_count.rb
+++ bin/git_delete_count.rb
@@ -37,120 +37,68 @@ module GitDeleteCount
     end
 
     def damage_string(damage)
-      if damage <= 0
-        ''
-      elsif damage <= 4
-        'pathetic'
-      elsif damage <= 8
-        'pitiful'
-      elsif damage <= 12
-        'feeble'
-      elsif damage <= 15
-        'weak'
-      elsif damage <= 18
-        'paltry'
-      elsif damage <= 22
-        'inadequate'
-      elsif damage <= 25
-        'mediocre'
-      elsif damage <= 31
-        ''
-      elsif damage <= 37
-        'average'
-      elsif damage <= 45
-        'telling'
-      elsif damage <= 52
-        'strong'
-      elsif damage <= 61
-        'POTENT'
-      elsif damage <= 75
-        'FORCEFUL'
-      elsif damage <= 85
-        'POWERFUL'
-      elsif damage <= 95
-        'FIERCE'
-      elsif damage <= 110
-        '** VICIOUS **'
-      elsif damage <= 125
-        '*** BRUTAL ***'
-      elsif damage <= 140
-        '*** MIGHTY ***'
-      elsif damage <= 160
-        '=== FEARSOME ==='
-      elsif damage <= 180
-        '=== FEROCIOUS ==='
-      elsif damage <= 220
-        '>>> FORMIDABLE <<<'
-      elsif damage <= 260
-        '<<< TREMENDOUS >>>'
-      elsif damage <= 320
-        '))) HEROIC ((('
-      elsif damage <= 400
-        ']]] TITANIC [[['
+      case
+      when damage <= 0 then ''
+      when damage <= 4 then 'pathetic'
+      when damage <= 8 then 'pitiful'
+      when damage <= 12 then 'feeble'
+      when damage <= 15 then 'weak'
+      when damage <= 18 then 'paltry'
+      when damage <= 22 then 'inadequate'
+      when damage <= 25 then 'mediocre'
+      when damage <= 31 then ''
+      when damage <= 37 then 'average'
+      when damage <= 45 then 'telling'
+      when damage <= 52 then 'strong'
+      when damage <= 61 then 'POTENT'
+      when damage <= 75 then 'FORCEFUL'
+      when damage <= 85 then 'POWERFUL'
+      when damage <= 95 then 'FIERCE'
+      when damage <= 110 then '** VICIOUS **'
+      when damage <= 125 then '*** BRUTAL ***'
+      when damage <= 140 then '*** MIGHTY ***'
+      when damage <= 160 then '=== FEARSOME ==='
+      when damage <= 180 then '=== FEROCIOUS ==='
+      when damage <= 220 then '>>> FORMIDABLE <<<'
+      when damage <= 260 then '<<< TREMENDOUS >>>'
+      when damage <= 320 then '))) HEROIC ((('
+      when damage <= 400 then ']]] TITANIC [[['
       else
         '### GODLIKE ###'
       end
     end
 
     def hit_string(damage)
-      if damage <= 1
-        'barely scratches'
-      elsif damage <= 2
-        'scratches'
-      elsif damage <= 4
-        'grazes'
-      elsif damage <= 8
-        'hurts'
-      elsif damage <= 12
-        'hits'
-      elsif damage <= 16
-        'injures'
-      elsif damage <= 22
-        'wounds'
-      elsif damage <= 30
-        'damages'
-      elsif damage <= 40
-        'harms'
-      elsif damage <= 52
-        'tears into'
-      elsif damage <= 66
-        'rends'
-      elsif damage <= 81
-        'mauls'
-      elsif damage <= 97
-        'savages'
-      elsif damage <= 114
-        'mutilates'
-      elsif damage <= 140
-        'maims'
-      elsif damage <= 180
-        'mangles'
-      elsif damage <= 220
-        'devastates'
-      elsif damage <= 280
-        'dismembers'
-      elsif damage <= 240
-        'ravages'
-      elsif damage <= 400
-        'sunders'
-      elsif damage <= 600
-        'kills'
-      elsif damage <= 800
-        'slays'
-      elsif damage <= 1600
-        'butchers'
-      elsif damage <= 2400
-        'slaughters'
-      elsif damage <= 3200
-        'exterminates'
-      elsif damage <= 4000
-        'destroys'
-      elsif damage <= 8000
-        'utterly destroys'
-      elsif damage <= 12_000
-        'annihilates'
-      elsif damage <= 20_000
-        'eradicates'
+      case
+      when damage <= 1 then 'barely scratches'
+      when damage <= 2 then 'scratches'
+      when damage <= 4 then 'grazes'
+      when damage <= 8 then 'hurts'
+      when damage <= 12 then 'hits'
+      when damage <= 16 then 'injures'
+      when damage <= 22 then 'wounds'
+      when damage <= 30 then 'damages'
+      when damage <= 40 then 'harms'
+      when damage <= 52 then 'tears into'
+      when damage <= 66 then 'rends'
+      when damage <= 81 then 'mauls'
+      when damage <= 97 then 'savages'
+      when damage <= 114 then 'mutilates'
+      when damage <= 140 then 'maims'
+      when damage <= 180 then 'mangles'
+      when damage <= 220 then 'devastates'
+      when damage <= 280 then 'dismembers'
+      when damage <= 240 then 'ravages'
+      when damage <= 400 then 'sunders'
+      when damage <= 600 then 'kills'
+      when damage <= 800 then 'slays'
+      when damage <= 1600 then 'butchers'
+      when damage <= 2400 then 'slaughters'
+      when damage <= 3200 then 'exterminates'
+      when damage <= 4000 then 'destroys'
+      when damage <= 8000 then 'utterly destroys'
+      when damage <= 12_000 then 'annihilates'
+      when damage <= 20_000 then 'eradicates'
       else
         'obliterates'
       end
