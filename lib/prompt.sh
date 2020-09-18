d86fc04d9f05b25b80f05ccbcfb2a99b3c54f6c9
Shane Cavanaugh <shane@shanecav.net>
2020-09-18 10:23:24 -0600

    Make colors available to whole file

--- lib/prompt.sh
+++ lib/prompt.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+cyan="\001$(tput setaf 6)\002"
+red="\001$(tput setaf 1)\002"
+purple="\001$(tput setaf 5)\002"
+yellow="\001$(tput setaf 3)\002"
+green="\001$(tput setaf 2)\002"
+reset_color="\001$(tput sgr0)\002"
+
 # https://gist.github.com/jstn/d249cc4bbb76e0e06ccc6d6a11ff5f04
 function git_prompt() {
   local dirty=false
@@ -21,18 +28,11 @@ function git_prompt() {
   fi
 
   if [ "$dirty" = true ]; then
-    echo "@$Yellow${ref#refs/heads/}$Color_Off"
+    echo "@$yellow${ref#refs/heads/}$reset_color"
   else
     echo "@${ref#refs/heads/}"
   fi
 }
-
-cyan="\001$(tput setaf 6)\002"
-red="\001$(tput setaf 1)\002"
-purple="\001$(tput setaf 5)\002"
-yellow="\001$(tput setaf 3)\002"
-green="\001$(tput setaf 2)\002"
-reset_color="\001$(tput sgr0)\002"
 
 function prompt() {
     PS1="\@ $cyan\W$reset_color$(git_prompt) $red>$reset_color$purple>$reset_color$yellow>$reset_color$green>$reset_color$cyan>$reset_color "
