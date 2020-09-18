44b8ee226b65f36b47c06962d040148bfae096af
Shane Cavanaugh <shane@shanecav.net>
2020-09-18 10:23:31 -0600

    Re-add bash completion

--- profile.sh
+++ profile.sh
@@ -5,3 +5,8 @@ for f in $DOTFILES_PATH/lib/*.sh; do source $f; done
 
 if which rbenv > /dev/null; then eval "$(rbenv init -)"; fi
 
+if [ -f /usr/local/opt/git/etc/bash_completion.d/git-completion.bash ]; then
+  . /usr/local/opt/git/etc/bash_completion.d/git-completion.bash
+else
+  echo "Expected this machine to have git-completion at /usr/local/opt/git/etc/bash_completion.d/git-completion.bash"
+fi
