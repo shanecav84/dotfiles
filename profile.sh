fb49f9401ebfc944981864ea37dd67007618ca7c
Shane Cavanaugh <shane@shanecav.net>
2025-11-05 16:18:56 -0700

    Add github credential helper

--- profile.sh
+++ profile.sh
@@ -2,7 +2,7 @@
 
 export PROMPT='%t [%F{cyan}%m%f] $(git_prompt_info)%B%F{cyan}%1~%f%b %F{red}>%F{yellow}>%F{green}>%F{cyan}>%F{blue}>%f '
 
-for f in $(ls $DOTFILES_PATH/lib/*.sh| sort -n); do source $f; done
+for f in $(ls $HOME/.dotfiles/lib/*.sh| sort -n); do source $f; done
 
 if type brew &>/dev/null; then
    fpath=($(brew --prefix)/share/zsh-completions $fpath)
