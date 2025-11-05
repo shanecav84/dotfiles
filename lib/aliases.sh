e32cfcd3a2fddafc3fc86847286972b2ba7db7ab
Shane Cavanaugh <shane@singlebrook.com>
2025-11-05 14:55:33 -0700

    Clean up aliases

--- lib/aliases.sh
+++ lib/aliases.sh
@@ -4,21 +4,11 @@
 alias reload!='. ~/.zshrc'
 
 # Update Homebrew
-alias brewup='brew upgrade && brew cask upgrade'
+alias brewup='brew upgrade && brew update'
 
-alias brc="echo -e '$YellowRunning Rails console in sandbox mode...$Color_Off'; bx rails c --sandbox"
-alias brs="bx rails s"
-alias bx='bundle exec'
-alias git-delete-count="$DOTFILES_PATH/bin/git_delete_count.rb"
-alias mine="open -a RubyMine"
 alias libyear="libyear-bundler"
-alias reset="clear && printf '\e[3J'"
 alias timestamp='date "+%Y%m%d%H%M%S"'
 
 # Overrides
 alias git=hub
-alias ls='ls -a'
-
-# Cuz i just can't anymore
-alias heroky=heroku
 
