ebc420a1244e1f1a8b5f2fee3e3beeb59051c9f9
Shane Cavanaugh <shane@singlebrook.com>
2025-12-16 10:32:27 -0700

    Remove alias

--- lib/aliases.sh
+++ lib/aliases.sh
@@ -6,7 +6,6 @@ alias reload!='. ~/.zshrc'
 # Update Homebrew
 alias brewup='HOMEBREW_NO_ENV_HINTS=1 brew upgrade && brew update'
 
-alias libyear="libyear-bundler"
 alias timestamp='date "+%Y%m%d%H%M%S"'
 
 # Overrides
