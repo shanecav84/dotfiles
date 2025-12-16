433ebee5ddd54f365d593639d6fd620affc89326
Shane Cavanaugh <shane@singlebrook.com>
2025-12-16 10:33:00 -0700

    Move git prompt to right

--- profile.sh
+++ profile.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env zsh
 
-export PROMPT='%t [%F{cyan}%m%f] %B%F{cyan}%1~%f%b $(git_prompt_info)%F{red}>%F{yellow}>%F{green}>%F{cyan}>%F{blue}>%f '
+export PROMPT='%t [%F{cyan}%m%f] %B%F{cyan}%1~%f%b %F{red}>%F{yellow}>%F{green}>%F{cyan}>%F{blue}>%f '
+
+RPROMPT='$(git_prompt_info)'
 
 for f in $(ls $HOME/.dotfiles/lib/*.sh| sort -n); do source $f; done
 
