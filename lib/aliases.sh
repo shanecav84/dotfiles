e2fd826f62f3040f9a55802f0e0724a43fdd489e
Shane Cavanaugh <shane@singlebrook.com>
2026-03-27 17:02:04 -0600

    Update timestamp format

--- lib/aliases.sh
+++ lib/aliases.sh
@@ -6,5 +6,5 @@ alias reload!='. ~/.zshrc'
 # Update Homebrew
 alias brewup='HOMEBREW_NO_ENV_HINTS=1 brew upgrade && brew update'
 
-alias timestamp='date "+%Y%m%d%H%M%S"'
+alias timestamp='date +"%Y-%m-%d %H:%M:%S %z"'
 
